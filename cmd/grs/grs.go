cbc845d7f72a410eca3722baecec270d65d65736
John Cheng <johnlicheng@gmail.com>
2018-03-30 17:03:11 -0700

    UX: Option to run in daemon mode

--- cmd/grs/grs.go
+++ cmd/grs/grs.go
@@ -4,19 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"jcheng/grs/config"
+	"jcheng/grs/display"
 	"jcheng/grs/grs"
 	"jcheng/grs/grsdb"
 	"jcheng/grs/script"
 	"jcheng/grs/status"
 	"os"
+	"os/signal"
 	"time"
-	"jcheng/grs/display"
 )
 
 type Args struct {
 	repos   string
 	verbose bool
 	command string
+	daemon  bool
+	refresh int
 }
 
 func main() {
@@ -25,6 +28,8 @@ func main() {
 	flag.StringVar(&args.repos, "repos", "", "target repos")
 	flag.StringVar(&args.command, "command", "", "command to run")
 	flag.BoolVar(&args.verbose, "verbose", false, "verbosity")
+	flag.BoolVar(&args.daemon, "d", false, "[daemon mode] enable daemon mode")
+	flag.IntVar(&args.refresh, "r", 300, "[daemon mode] How often to check for changes, in seconds.")
 	flag.Parse()
 
 	if args.verbose {
@@ -61,40 +66,57 @@ func main() {
 		ctx.SetDB(db)
 	}
 
-
 	var screen display.Display = display.NewAnsiDisplay(os.Stdout)
-
 	var repoStatusList = make([]display.RepoStatus, len(repos))
-	for idx, repo := range repos {
-		rstat := status.NewRStat()
-		script.BeforeScript(ctx, repo, runner, rstat)
-		if rstat.Dir == status.DIR_VALID {
-			script.Fetch(ctx, runner, rstat, repo)
-		}
-		if rstat.Dir == status.DIR_VALID {
-			script.GetRepoStatus(ctx, runner, rstat)
 
+	ctrl := make(chan os.Signal, 1)
+	signal.Notify(ctrl, os.Interrupt)
+	go func() {
+		for sig := range ctrl {
+			grs.Debug("got %v, quitting", sig)
+			os.Exit(0)
 		}
-		if rstat.Dir == status.DIR_VALID {
-			script.GetIndexStatus(ctx, runner, rstat)
-		}
+	}()
 
-		merged := false
-		if atime, err := script.GetActivityTime(repo.Path); err == nil && time.Now().After(atime.Add(ctx.ActivityTimeout)) {
-			merged = script.AutoFFMerge(ctx, runner, rstat)
-		}
+	var repeat = true
+	for repeat {
+		for idx, repo := range repos {
+			rstat := status.NewRStat()
+			script.BeforeScript(ctx, repo, runner, rstat)
+			if rstat.Dir == status.DIR_VALID {
+				script.Fetch(ctx, runner, rstat, repo)
+			}
+			if rstat.Dir == status.DIR_VALID {
+				script.GetRepoStatus(ctx, runner, rstat)
 
-		if repoPtr := ctx.DB().FindRepo(repo.Path); repoPtr != nil {
-			repoPtr.RStat.Update(*rstat)
-		}
+			}
+			if rstat.Dir == status.DIR_VALID {
+				script.GetIndexStatus(ctx, runner, rstat)
+			}
 
-		repoStatusList[idx] = display.RepoStatus{
-			Path: repo.Path,
-			Rstat: *rstat,
-			Merged: merged,
+			merged := false
+			if atime, err := script.GetActivityTime(repo.Path); err == nil && time.Now().After(atime.Add(ctx.ActivityTimeout)) {
+				merged = script.AutoFFMerge(ctx, runner, rstat)
+			}
+
+			if repoPtr := ctx.DB().FindRepo(repo.Path); repoPtr != nil {
+				repoPtr.RStat.Update(*rstat)
+			}
+
+			repoStatusList[idx] = display.RepoStatus{
+				Path:   repo.Path,
+				Rstat:  *rstat,
+				Merged: merged,
+			}
 		}
+		grsdb.SaveFile(ctx.DBWriter(), ctx.DbPath, ctx.DB())
+		screen.SummarizeRepos(repoStatusList)
+		screen.Update()
+
+		if !args.daemon {
+			repeat = false
+			continue
+		}
+		time.Sleep(time.Second * time.Duration(args.refresh))
 	}
-	grsdb.SaveFile(ctx.DBWriter(), ctx.DbPath, ctx.DB())
-	screen.SummarizeRepos(repoStatusList)
-	screen.Update()
 }
