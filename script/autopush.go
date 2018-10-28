df7102fe16abeab6424b30e578c2eaa446741919
John Cheng <johnlicheng@gmail.com>
2018-10-27 21:39:08 -0700

    wip: scaffolding for autopushing

--- script/autopush.go
+++ script/autopush.go
@@ -1,13 +1,14 @@
 package script
 
-// AutoPush runs `git push...` when the branch is ahead and unmofified
+// AutoPush runs `git push...` when the branch is ahead. It also autocommits changes.
 func (s *Script) AutoPush() bool {
 	repo := s.repo
 	ctx := s.ctx
 	if s.err != nil ||
 		repo.Dir != DIR_VALID ||
 		repo.Branch != BRANCH_AHEAD ||
-		repo.Index != INDEX_UNMODIFIED {
+		repo.Index != INDEX_UNKNOWN ||
+		!repo.PushAllowed {
 		return false
 	}
 
--- script/autopush_test.go
+++ script/autopush_test.go
@@ -1,17 +1,18 @@
 package script
 
 import (
+	"jcheng/grs/shexec"
 	"testing"
 )
 
-// TODO JCHENG
+// Verify that git push does not get called when repo status is unexpected
 func TestAutoPush_Noop(t *testing.T) {
-	verify_AutoPush_NoGitExec(t, DIR_INVALID, BRANCH_BEHIND, INDEX_UNMODIFIED)
+	verify_AutoPush_NoGitExec(t, DIR_INVALID, BRANCH_AHEAD, INDEX_UNKNOWN)
 
-	verify_AutoPush_NoGitExec(t, DIR_VALID, BRANCH_AHEAD, INDEX_UNMODIFIED)
+	verify_AutoPush_NoGitExec(t, DIR_VALID, BRANCH_AHEAD, INDEX_MODIFIED)
 	verify_AutoPush_NoGitExec(t, DIR_VALID, BRANCH_UNKNOWN, INDEX_UNMODIFIED)
 
-	verify_AutoPush_NoGitExec(t, DIR_VALID, BRANCH_BEHIND, INDEX_UNKNOWN)
+	verify_AutoPush_NoGitExec(t, DIR_VALID, BRANCH_BEHIND, INDEX_UNMODIFIED)
 	verify_AutoPush_NoGitExec(t, DIR_VALID, BRANCH_BEHIND, INDEX_MODIFIED)
 }
 
@@ -20,7 +21,20 @@ func TestAutoPush_Ok(t *testing.T) {
 
 }
 
-// TODO JCHENG
+// Given the Dirstat, Branchstat, and Indexstat, signal an error if git push was called
 func verify_AutoPush_NoGitExec(t *testing.T, dir Dirstat, branch Branchstat, index Indexstat) {
+	runner := shexec.NewMockRunner()
+	runner.AddMap("git", shexec.Ok(""))
 
+	ctx := shexec.NewAppContextWithRunner(runner)
+
+	repo := NewRepo("")
+	repo.Dir = dir
+	repo.Branch = branch
+	repo.Index = index
+	NewScript(ctx, repo).AutoPush()
+
+	if runner.HistoryCount("git push") != 0 {
+		t.Errorf("unexpected `git push` given dirstat=%v, branchstat=%v, indexstat=%v\n", dir, branch, index)
+	}
 }
